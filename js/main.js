c79a74a116878f7e65cb5bff54140fcd91d4f519
freelancersazal <freelancersazal@gmail.com>
2023-08-14 19:31:26 +0600

    banner added

--- js/main.js
+++ js/main.js
@@ -1,23 +1,32 @@
 $(function () {
-    
-    "use strict";
-    
-    
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    "use strict";
+
+
+    //=======MENU FIX JS=======   
+    if ($('.main_menu').offset() != undefined) {
+        var navoff = $('.main_menu').offset().top;
+        $(window).scroll(function () {
+            var scrolling = $(this).scrollTop();
+
+            if (scrolling > navoff) {
+                $('.main_menu').addClass('menu_fix');
+            } else {
+                $('.main_menu').removeClass('menu_fix');
+            }
+        });
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
 });
