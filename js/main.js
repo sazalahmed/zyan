f4b394f5dc665f589640b6061b7f8f6969249271
freelancersazal <freelancersazal@gmail.com>
2023-08-18 00:43:07 +0600

    added three new pages

--- js/main.js
+++ js/main.js
@@ -18,15 +18,78 @@ $(function () {
     }
 
 
+    //=====venobox js=====
+    $('.venobox').venobox();
 
 
+    //=====counter js=======   
+    $('.counter').countUp();
 
 
+    //====BARFILLER JS========
+    $(document).ready(function () {
+        $('#bar1').barfiller({ barColor: '#55E6A5' });
+        $('#bar2').barfiller({ barColor: '#55E6A5' });
+        $('#bar3').barfiller({ barColor: '#55E6A5' });
+        $('#bar4').barfiller({ barColor: '#55E6A5' });
+        $('#bar5').barfiller({ barColor: '#55E6A5' });
+        $('#bar6').barfiller({ barColor: '#55E6A5' });
+    });
 
 
+    //======TESTIMONIAL SLIDER======   
+    $('.testi_slider').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: false,
+        arrows: false,
+
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
 
 
+    //======BRAND SLIDER======   
+    $('.marquee_animi').marquee({
+        speed: 100,
+        gap: 0,
+        delayBeforeStart: 0,
+        direction: 'left',
+        duplicated: true,
+        pauseOnHover: true
+    });
 
+
+    //======STICKY SIDEBAR======
+    $("#sticky_sidebar").stickit({
+        top: 100,
+    })
 
 
 });
